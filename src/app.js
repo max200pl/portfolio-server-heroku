32266536d7bfa726a704b0dcab2f15d857c82e4b
max200pl <max2000pl@gmail.com>
2024-08-13 18:04:11 +0300

    #updated cors

--- src/app.js
+++ src/app.js
@@ -3,12 +3,21 @@ const cors = require("cors");
 const api = require("./routes/api");
 const app = express();
 
+const allowedOrigins = [
+    "http://localhost:3000",
+    "https://maksym-poskanny-portfolio-react.onrender.com",
+    "https://portfolio-server-little-lake-1018.fly.dev",
+];
+
 app.use(
     cors({
-        origin: [
-            "http://localhost:3000",
-            "https://maksym-poskanny-portfolio-react.onrender.com",
-        ],
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
