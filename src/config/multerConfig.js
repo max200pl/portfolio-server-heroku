b46e7e9cee95d29e7b05b5839c174bb57a898fea
max200pl <max2000pl@gmail.com>
2024-09-23 10:16:03 +0300

    #fixed indents

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -1,35 +1,49 @@
 // multerConfig.js
-const multer = require('multer');
-const { join, extname } = require('path');
-const fs = require('fs');
-const { toCamelCase } = require('../helpers/helpers');
+const multer = require("multer");
+const { join, extname } = require("path");
+const fs = require("fs");
+const { toCamelCase } = require("../helpers/helpers");
 
-const SRC_IMAGE_FOLDER = "../images/"
+const SRC_IMAGE_FOLDER = "../images/";
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const folderPath = join(__dirname, SRC_IMAGE_FOLDER, toCamelCase(req.body.name))
+    destination: (req, file, callback) => {
+        const folderPath = join(
+            __dirname,
+            SRC_IMAGE_FOLDER,
+            toCamelCase(req.body.name)
+        );
 
         if (!fs.existsSync(folderPath)) {
-            fs.mkdirSync(folderPath, { recursive: true })
+            fs.mkdirSync(folderPath, { recursive: true });
         }
 
-        cb(null, folderPath)
+        callback(null, folderPath);
     },
-    filename: (req, file, cb) => {
-        const filename = 'intro';
+    filename: (req, file, callback) => {
+        const filename = "intro";
         const fileExtension = extname(file.originalname);
 
         // Remove any existing files with the name "intro" in the specified directory
-        const filesToRemove = fs.readdirSync(join(__dirname, SRC_IMAGE_FOLDER, toCamelCase(req.body.name)))
-            .filter(existingFile => existingFile.startsWith(filename));
+        const filesToRemove = fs
+            .readdirSync(
+                join(__dirname, SRC_IMAGE_FOLDER, toCamelCase(req.body.name))
+            )
+            .filter((existingFile) => existingFile.startsWith(filename));
 
-        filesToRemove.forEach(existingFile => {
-            fs.unlinkSync(join(__dirname, SRC_IMAGE_FOLDER, toCamelCase(req.body.name), existingFile));
+        filesToRemove.forEach((existingFile) => {
+            fs.unlinkSync(
+                join(
+                    __dirname,
+                    SRC_IMAGE_FOLDER,
+                    toCamelCase(req.body.name),
+                    existingFile
+                )
+            );
         });
 
-        cb(null, filename + fileExtension);
-    }
+        callback(null, filename + fileExtension);
+    },
 });
 
 const upload = multer({ storage: storage });
