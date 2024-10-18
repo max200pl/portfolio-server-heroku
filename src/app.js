ec8a0772fd26a9307b12dcfb9a9ceac0c97dbacd
max200pl <max2000pl@gmail.com>
2024-10-18 11:58:58 +0300

    #commented out  logs

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const allowedOrigins = [
 
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
-    console.log("Headers:", req.headers);
+    // console.log("Headers:", req.headers);
     next();
 });
 
