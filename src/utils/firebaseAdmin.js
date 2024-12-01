66254ba927fc06ce03f18cebaa27ac49373b8474
max200pl <max2000pl@gmail.com>
2024-12-01 10:21:12 +0200

    #fix

--- src/utils/firebaseAdmin.js
+++ src/utils/firebaseAdmin.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 
 const serviceAccount = {
     type: "service_account",
-
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
     private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
