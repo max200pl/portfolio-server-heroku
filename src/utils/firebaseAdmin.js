bce1a42e395291492109bccb75eed0daa7ae5693
max200pl <max2000pl@gmail.com>
2024-12-02 10:11:11 +0200

    #dotenv #fixed sensitive content

--- src/utils/firebaseAdmin.js
+++ src/utils/firebaseAdmin.js
@@ -1,16 +1,30 @@
 const admin = require("firebase-admin");
-const path = require("path");
+require("dotenv").config();
 
-const serviceAccount = require(path.join(
-    __dirname,
-    "../../firebase-adminsdk.json"
-));
+const serviceAccount = {
+    type: "service_account",
+
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url:
+        process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    storageBucket: "portfolio-react-5b7d3.appspot.com",
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
 
 const bucket = admin.storage().bucket();
 
-module.exports = { bucket };
+module.exports = {
+    bucket,
+};
--- src/utils/firebaseStorage.js
+++ src/utils/firebaseStorage.js
@@ -1,8 +1,9 @@
 const { ref, uploadBytes, getDownloadURL } = require("firebase/storage");
+const { bucket } = require("./firebaseAdmin"); // Import bucket from firebaseAdmin
 
 async function uploadImageToFirebase(file) {
     try {
-        const storageRef = ref(storage, `images/${file.originalname}`);
+        const storageRef = ref(bucket, `images/${file.originalname}`);
         const snapshot = await uploadBytes(storageRef, file.buffer);
         const downloadURL = await getDownloadURL(snapshot.ref);
         console.log("Image uploaded to Firebase:", downloadURL);
