49e57a2922479d884589cfe4ecbcec752c4ef741
max200pl <max2000pl@gmail.com>
2024-12-22 14:55:39 +0200

    refactor category field in work schema to use ObjectId reference for improved data integrity

--- src/db/works.mongo.js
+++ src/db/works.mongo.js
@@ -19,7 +19,11 @@ const workSchema = new mongoose.Schema(
     {
         name: { type: String, required: true },
         dateFinished: { type: Date, required: true },
-        category: { type: String, required: true },
+        category: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "CategoryWork",
+        },
         client: { type: String, default: undefined },
         link: { type: String, default: undefined },
         frontTech: { type: Map, of: [techSchema], default: undefined },
