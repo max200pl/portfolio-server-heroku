a01f3c26da1b2390f2bc21d7e189719f73710ac9
max200pl <max2000pl@gmail.com>
2025-02-17 10:09:44 +0200

    refactor: correct variable naming for login and logout routers in auth router

--- src/routes/auth/auth.router.js
+++ src/routes/auth/auth.router.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const authLogInRouter = require("./authLogin.router");
-const authLogOutRouter = require("./authLogout.router");
+const authLoginRouter = require("./authLogin.router");
+const authLogoutRouter = require("./authLogout.router");
 const authSignUpRouter = require("./authSignUp.router");
 const authRouter = express.Router();
 
 authRouter.use("/sign-up", authSignUpRouter);
-authRouter.use("/login", authLogInRouter);
-authRouter.use("/logout", authLogOutRouter);
+authRouter.use("/login", authLoginRouter);
+authRouter.use("/logout", authLogoutRouter);
 
 module.exports = {
     authRouter,
